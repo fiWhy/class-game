7180b71e873394e1d5b1d55da6952a27f7b7f55b
Yasha33 <57111386+Yasha33@users.noreply.github.com>
2019-12-09 19:40:19 +0200

    Delete main-unit.js

--- main-unit.js
+++
@@ -1,9 +0,0 @@
-var MainUnit = (function () {
-    function MainUnit(health) {
-        this.health = this.MaxHealth = health;
-    }
-    MainUnit.prototype.move = function () {
-        console.log('Person', this, 'move');
-    };
-    return MainUnit;
-}());
