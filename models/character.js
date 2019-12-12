1d47b080b2c2d40e97b93ea0d2f68e8acdcbc5bc
Denis Buryachkovskiy <zekatsi5@gmail.com>
2019-12-12 19:20:39 +0200

    Added monsters and character as script list

---
+++ models/character.js
@@ -0,0 +1,11 @@
+var Character = (function () {
+  function Character(level) {
+      Unit.call(this, 600, 1000, level);
+  }
+
+  Character.prototype = Object.create(Unit.prototype);
+  Character.prototype.constructor = Character;
+
+
+  return Character;
+}());
