450ee6347ce55c873400a3b51f7b96fc7d15a9f3
Denis Buryachkovskiy <zekatsi5@gmail.com>
2019-12-19 19:42:29 +0200

    Added controls and cells wrapper

--- models/battleground.js
+++ models/battleground.js
@@ -1,14 +1,14 @@
 var BattleGround = (function() {
-  function BattleGround(size, amountOfMonsters) {
+  function BattleGround(element, size, amountOfMonsters) {
     this.amountOfMonsters = amountOfMonsters || size / 2;
     this.area = new Array(size).fill(1).map(function() {
       return new Grass();
     });
+    this.render();
   }
- 
+
   BattleGround.prototype.fill = function(monstersArr) {
     var self = this;
-    console.log(monstersArr);
     Array(this.amountOfMonsters)
       .fill(1)
       .forEach(function() {
@@ -18,9 +18,21 @@ var BattleGround = (function() {
       });
   };
 
+  BattleGround.prototype.render = function() {
+    var el = document.createElement('div');
+    el.classList.add('game__wrapper');
+    el.innerHTML =
+      '<div class="game__cells"></div> \
+        <div class="game__controls"></div>\
+      ';
+
+    var cellsElement = el.querySelector('.game__cells'),
+      controlsElement = el.querySelector('.game__controls');
+  };
+
   BattleGround.prototype.addCharacter = function(character) {
     this.area[0] = character;
-  }
- 
+  };
+
   return BattleGround;
 })();
--- script.js
+++ script.js
@@ -2,6 +2,3 @@ var bg = new BattleGround(document.getElementById('wrapper'), 30);
 
 bg.fill([Dragon, CrocoTurtle, Monkey, Snake, MonsterBear]);
 bg.addCharacter(new Character());
-
-var rootElement = document.getElementById('rootElement');
-consoleNodesRecursive(rootElement);
