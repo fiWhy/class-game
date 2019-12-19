dff089fa2e16a7e98e0983b12ef2ef544c07c4ae
Anastasia <shvets.anastasia04@gmail.com>
2019-12-19 20:23:56 +0200

    CELLS

--- utils/dom.js
+++ utils/dom.js
@@ -1,5 +1,4 @@
 function consoleNodesRecursive(root) {
-<<<<<<< HEAD
     root.childNodes.forEach(function(child){
         if (child.childNodes && child.childNodes.length) {
             console.group('element ' + child.tagName)
@@ -12,20 +11,3 @@ function consoleNodesRecursive(root) {
         }
     });
 }
-=======
-  console.dir(root);
-  root.childNodes.forEach(function(child) {
-    if (child.childNodes && child.childNodes.length) {
-      console.group("ELEMENT " + child.tagName);
-      consoleNodesRecursive(child);
-      console.groupEnd();
-    } else {
-      var value =
-        child.nodeType !== Node.TEXT_NODE && child.tagName !== "SCRIPT"
-          ? child.nodeValue.trim() || ""
-          : "";
-      console.log(child.nodeName, value);
-    }
-  });
-}
->>>>>>> master
