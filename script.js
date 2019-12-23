c7741836497e382c0ec1fd5f9734278621f05cb7
Denis Buryachkovskiy <zekatsi5@gmail.com>
2019-12-23 19:43:35 +0200

    Added methods

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ var bg = new BattleGround(document.getElementById('wrapper'), 30);
 
 bg.fill([Dragon, CrocoTurtle, Monkey, Snake, MonsterBear]);
 bg.addCharacter(new Character());
+
+var fnc = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      bg.moveCharacterLeft();
+      break;
+    case 38:
+      bg.characterJump();
+      break;
+    case 39:
+      bg.moveCharacterRight();
+      break;
+    case 40:
+      bg.characterCrouch();
+      break;
+  }
+};
+
+document.addEventListener('keydown', fnc);
