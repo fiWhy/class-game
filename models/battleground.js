7a93cf5ea9ba158c4bd4d5cb9144fd090e3d8ffd
Anastasia <shvets.anastasia04@gmail.com>
2019-12-23 19:45:49 +0200

    move

--- models/battleground.js
+++ models/battleground.js
@@ -1,3 +1,56 @@
+// var BattleGround = (function() {
+//   function BattleGround(element, size, amountOfMonsters) {
+//     this.wrapperElement = null;
+//     this.cellsElement = null;
+//     this.controllsElement = null;
+//     this.element = element;
+//     this.size = size;
+//     this.amountOfMonsters = amountOfMonsters || size / 2;
+//     this.area = new Array(size).fill(1).map(function() {
+//       return new Grass();
+//     });
+//     this.render();
+//     this.update();
+//   }
+
+//   BattleGround.prototype.fill = function(monstersArr) {
+//     var self = this;
+//     Array(this.amountOfMonsters)
+//       .fill(1)
+//       .forEach(function() {
+//         self.area[random(1, self.area.length)] = new monstersArr[
+//           random(0, monstersArr.length)
+//         ](random(1, 3));
+//       });
+
+//     this.update();
+//   };
+
+//   BattleGround.prototype.render = function() {
+//     var ul = document.querySelector('.ul');
+
+//     this.area.forEach(function(el) {
+//       ul.append
+//     })
+//     for (var i = 1; i < this.size; i++) {
+//         var li = document.createElement('li')
+//         li.innerHTML 
+//         ul.append(li)
+//     }
+// };
+
+//   BattleGround.prototype.addCharacter = function(character) {
+//     this.area[0] = character;
+//   };
+
+//   return BattleGround;
+// })();
+
+
+
+
+
+
 var BattleGround = (function() {
   function BattleGround(element, size, amountOfMonsters) {
     this.wrapperElement = null;
@@ -27,18 +80,38 @@ var BattleGround = (function() {
   };
 
   BattleGround.prototype.render = function() {
-    var ul = document.querySelector('.ul');
+    this.wrapper = document.createElement('div');
+    this.wrapper.classList.add('game__wrapper');
+    this.wrapper.innerHTML =
+      '<div class="game__cells"></div> \
+        <div class="game__controls"></div>\
+      ';
 
-    for (var i = 1; i < this.size; i++) {
-        var li = document.createElement('li')
-        li.innerHTML 
-        ul.append(li)
-    }
-};
+    this.cellsElement = this.wrapper.querySelector('.game__cells');
+    this.controllsElement = this.wrapper.querySelector('.game__controls');
+    this.wrapper.appendChild(this.cellsElement);
+    this.wrapper.appendChild(this.controllsElement);
+    this.element.appendChild(this.wrapper);
+  };
+
+  BattleGround.prototype.update = function() {
+    var self = this;
+    this.cellsElement.innerHTML = '';
+    this.area.forEach(function(el) {
+      var cell = document.createElement('div');
+      var cellElement = el.render();
+
+      cell.classList.add('game__cells__item');
+      cell.appendChild(cellElement);
+      self.cellsElement.appendChild(cell);
+    });
+  };
 
   BattleGround.prototype.addCharacter = function(character) {
     this.area[0] = character;
   };
+
+  
 
   return BattleGround;
 })();
--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-var bg = new BattleGround(document.getElementsByClassName('ul'), 30);
+var bg = new BattleGround(document.getElementById('wrapper'), 30);
 
 bg.fill([Dragon, CrocoTurtle, Monkey, Snake, MonsterBear]);
 bg.addCharacter(new Character());
+
+var fnc = function(e) {
+    switch(e.keyCode) {
+        case 37:
+            bg.moveCharacterLeft()
+            console.log('Left');
+            break;
+        case 38:
+            bg.moveCharacterJump();
+            console.log('Up')
+            break;
+        case 39:
+            bg.moveCharacterRight();
+            console.log('Right')
+            break;
+        case 40:
+            bg.moveCharacterCrouch();
+            console.log('Down')
+            break;
+    }
+}
+
+document.addEventListener('keydown', fnc)
 
 
 // BattleGround.prototype.render = function() {
