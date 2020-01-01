59ef78dbdfd04d8ce363d9d3a2b336b1ba0a3efb
igerashchenko <igerashchenko@determine.com>
2020-01-01 12:37:16 +0200

    Modify css file

--- models/battleground.js
+++ models/battleground.js
@@ -57,34 +57,6 @@ var BattleGround = (function() {
     this.drawElements();
   }
 
-  // BattleGround.prototype.render = function() {
-  //   this.wrapper = document.createElement('div');
-  //   this.wrapper.classList.add('game__wrapper');
-  //   this.wrapper.innerHTML =
-  //     '<div class="game__cells"></div> \
-  //       <div class="game__controls"></div>\
-  //     ';
-
-  //   this.cellsElement = this.wrapper.querySelector('.game__cells');
-  //   this.controllsElement = this.wrapper.querySelector('.game__controls');
-  //   this.wrapper.appendChild(this.cellsElement);
-  //   this.wrapper.appendChild(this.controllsElement);
-  //   this.element.appendChild(this.wrapper);
-  // };
-
-  // BattleGround.prototype.update = function() {
-  //   var self = this;
-  //   this.cellsElement.innerHTML = '';
-  //   this.area.forEach(function(el) {
-  //     var cell = document.createElement('div');
-  //     var cellElement = el.render();
-
-  //     cell.classList.add('game__cells__item');
-  //     cell.appendChild(cellElement);
-  //     self.cellsElement.appendChild(cell);
-  //   });
-  // };
-
   BattleGround.prototype.addCharacter = function(character) {
     this.beforeElement = this.area[this.characterIndex];
     this.area[this.characterIndex] = this.character = character;
